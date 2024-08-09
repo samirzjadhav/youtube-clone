36ec8378e09d983fe814f155f93970e9968f8ef1
samir jadhav <samirzjadhav@gmail.com>
2024-08-09 20:56:24 +0530

    feat: complete header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
   const { loading, mobileMenu, setMobileMenu } = useContext(Context);
 
   const navigate = useNavigate();
-  const serarchQueryHandler = (event) => {
+  const searchQueryHandler = (event) => {
     if (
       (event?.key === "Enter" || event === "searchButton") &&
       searchQuery?.length > 0
@@ -71,13 +71,29 @@ const Header = () => {
             className="bg-transparent outline-none text-white pr-5 pl-5 md:pl-0 w-44 md:group-focus-within:pl-0 md:w-64 lg:w-[500px]"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
-            onKeyUp={serarchQueryHandler}
+            onKeyUp={searchQueryHandler}
           />
         </div>
-        <button className="w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] rounded-r-3xl bg-white/[0.1]">
+        <button
+          className="w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] rounded-r-3xl bg-white/[0.1]"
+          onClick={() => searchQueryHandler("searchButton")}
+        >
           {" "}
           <IoIosSearch className="text-white text-xl" />
         </button>
+      </div>
+      <div className="flex items-center">
+        <div className="hidden md:flex">
+          <div className="flex items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
+            <RiVideoAddLine className="text-white text-xl cursor-pointer" />
+          </div>
+          <div className="flex items-center justify-center ml-2 h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
+            <FiBell className="text-white text-xl cursor-pointer" />
+          </div>
+        </div>
+        <div className="flex h-8 w-8 overflow-hidden rounded-full md:ml-4">
+          <img src="https://xsgames.co/randomusers/assets/avatars/female/68.jpg" />
+        </div>
       </div>
     </div>
   );

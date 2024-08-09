63bd0f0f571bee92b8fc93ca09681beaade821d7
samir jadhav <samirzjadhav@gmail.com>
2024-08-09 20:47:43 +0530

    feat: added search bar on header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -60,6 +60,25 @@ const Header = () => {
           <img src={ytLogoMobile} alt="youtube" className="h-full md:hidden" />
         </Link>
       </div>
+      <div className="flex group items-center">
+        <div className="flex h-8 md:h-10 md:ml-10 md:pl-5  border-[#303030] border rounded-l-3xl group-focus-within:border-blue-500 md:group-focus-within:ml-5 md:group-focus-within:pl-0">
+          <div className="w-10 items-center justify-center hidden group-focus-within:md:flex">
+            <IoIosSearch className="text-white text-xl" />
+          </div>
+          <input
+            type="text"
+            // placeholder="Search"
+            className="bg-transparent outline-none text-white pr-5 pl-5 md:pl-0 w-44 md:group-focus-within:pl-0 md:w-64 lg:w-[500px]"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyUp={serarchQueryHandler}
+          />
+        </div>
+        <button className="w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] rounded-r-3xl bg-white/[0.1]">
+          {" "}
+          <IoIosSearch className="text-white text-xl" />
+        </button>
+      </div>
     </div>
   );
 };

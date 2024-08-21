dc47d540836b9f1d829960247b8a769bc4f5f9e3
samir jadhav <samirzjadhav@gmail.com>
2024-08-21 20:19:19 +0200

    feat: added serch result

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { fetchDataFromApi } from "../utils/api";
+import { Context } from "../context/contextApi";
+import LeftNav from "./LeftNav";
+import SearchResultVideoCard from "./SearchResultVideoCard";
 
 const SearchResult = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [result, setResult] = useState();
+  const { serchQuery } = useParams();
+  const { setLoading } = useContext(Context);
 
-export default SearchResult
+  useEffect(() => {}, [serchQuery]);
+
+  const fetchSearchResult = () => {
+    setLoading(true);
+    fetchDataFromApi(`search/?q=${serchQuery}`).then((res) => {
+      console.log(res);
+      setResult(res?.contents);
+      setLoading(false);
+    });
+  };
+
+  return <div></div>;
+};
+
+export default SearchResult;

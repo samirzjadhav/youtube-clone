98d5facf624e86017e29096af99162f15222bc2d
samir jadhav <samirzjadhav@gmail.com>
2024-08-24 20:19:19 +0200

    feat: complete search result section

--- src/components/SearchResultVideoCard.jsx
+++ src/components/SearchResultVideoCard.jsx
@@ -24,6 +24,35 @@ const SearchResultVideoCard = ({ video }) => {
           <span className="empty:hidden text-sm line-clamp-1 md:line-clamp-2 text-white/[0.7] md:pr-24 md:my-4">
             {video?.descriptionSnippet}
           </span>
+          <div className="hidden md:flex items -center">
+            <div className="flex item-start mr-3">
+              <div className="flex h-9 w-9 rounded-full overflow-hidden">
+                <img
+                  src={video?.author?.avatar[0]?.url}
+                  className="h-full w-full object-cover"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <span className="text-sm font-semibold mt-2 text-white/[0.7] flex items-center">
+                {video?.author?.title}
+                {video?.author?.badges[0]?.type === "VERIFIED_CHANNEL" && (
+                  <BsFillCheckCircleFill className="text-white/[0.5] text-[12px] lg:text-[10px] xl:text-[12px] ml-1" />
+                )}
+              </span>
+              <div className="flex text-sm font-semibold text-white/[0.7] truncate overflow-hidden">
+                <span>{`${abbreviateNumber(
+                  video?.stats?.views,
+                  2
+                )} views`}</span>
+                <span className="flex text-[24px] leading-none font-bold text-white/[0.7] relative top-[-10px] mx-1">
+                  .
+                </span>
+                <span className="truncate">{video?.publishedTimeText}</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Link>
